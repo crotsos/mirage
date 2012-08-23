82fec002d0ba9aff7afa427f854c22ff28e6d9c6
crotsos <cr409@cl.cam.ac.uk>
2012-08-23 11:13:06 -0700

    make mvar put wake up immediately

--- lib/std/lwt_mvar.ml
+++ lib/std/lwt_mvar.ml
@@ -60,7 +60,7 @@ let put mvar v =
           | None ->
               mvar.contents <- Some v
           | Some w ->
-              Lwt.wakeup_later w v
+              Lwt.wakeup w v
         end;
         return_unit
     | Some _ ->
@@ -75,7 +75,7 @@ let take mvar =
         begin match Lwt_sequence.take_opt_l mvar.writers with
           | Some(v', w) ->
               mvar.contents <- Some v';
-              Lwt.wakeup_later w ()
+              Lwt.wakeup w ()
           | None ->
               mvar.contents <- None
         end;
